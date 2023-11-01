c3852c4b527088b4b77b68d9e6e9c5ca5c230973
ChrisTarla <christine.tarla26@ycdsbk12.ca>
2023-11-01 09:47:57 -0400

    finished sections 1-3

--- Sketch.java
+++ Sketch.java
@@ -24,14 +24,6 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    /*
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-*/
     draw_section_outlines();
     draw_section1();
     draw_section2();
@@ -41,48 +33,40 @@ public class Sketch extends PApplet {
     draw_section5();
     draw_section6();
     draw_section7();
-    draw_section8();
-
-    
+    draw_section8();  
   }
 
-
-  /**
-   * Draw the outlines for all sections
-   */
+  // Draw the outlines for all sections 
   public void draw_section_outlines(){
     stroke(0);
     noFill();
 
-    // draw bottom row boxes
+    // Draw bottom row boxes
     rect(0,300, 300, 300);
     rect(300, 300, 300, 300);
     rect(600, 300, 300, 300);
     rect(900, 300, 300, 300);
 
-    // draw top row boxes
+    // Draw top row boxes
     rect(0,0, 300, 300);
     rect(300, 0, 300, 300);
     rect(600, 0, 300, 300);
     rect(900, 0, 300, 300);
   }
   
-  /**
-   * draws the bottom left section
-   */
+  // Draws the bottom left section 
   public void draw_section1(){
     int intX = 0;
     int intY = 0;
 
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+    for(int intRow = 0; intRow < 295; intRow += 6){
+      for(int intColumn = 0; intColumn < 600; intColumn += 6){
+        intX = 3 + intRow;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 300 + 3 + intColumn; //Instead of zero, calculate the proper intY location using 'intColumn'
 
         fill(255);
         noStroke();
-        rect(intX, intY, 5, 5);
-
+        rect(intX, intY, 3, 3); 
       }
     }
   }
@@ -92,7 +76,27 @@ public class Sketch extends PApplet {
    * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
    */
   public void draw_section2(){
+    int intX = 0;
+    int intY = 0;
+    int intFillColour = 0;
+    
+    for (int y = 300; y < 595; y += 6){
+      for (int x = 300; x < 595; x += 6){
+        intX = x + 3;
+        intY = y + 3;
+        
+        if (intFillColour == 0){
+          fill(255);
+          intFillColour += 1;
+        } else if (intFillColour == 1){
+          fill(30, 80, 120);
+          intFillColour -= 1;
+        }
+        
+        rect(intX, intY, 3, 3);
 
+      }
+    }
   }
 
   /**
@@ -100,7 +104,26 @@ public class Sketch extends PApplet {
    * Don't use multiple 'if' statements.
    */
   public void draw_section3(){
-
+    int intX = 0;
+    int intY = 0;
+    int intFillColour = 0;
+    
+    for (int x = 600; x < 895; x += 6){
+      for (int y = 300; y < 595; y += 6){
+        intX = x + 3;
+        intY = y + 3;
+        
+        if (intFillColour == 0){
+          fill(255);
+          intFillColour += 1;
+        } else if (intFillColour == 1){
+          fill(30, 80, 120);
+          intFillColour -= 1;
+        }
+        
+        rect(intX, intY, 3, 3);
+      }
+    }
   }
 
   /**
@@ -128,10 +151,5 @@ public class Sketch extends PApplet {
   public void draw_section8(){
 
   }
-
-
-
-
-
 
 }
